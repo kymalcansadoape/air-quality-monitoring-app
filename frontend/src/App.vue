fb409ec5c13cc4216a07efe1f4a531269d07f802
Duhell <ryanvillarma148@gmail.com>
2023-02-10 00:02:35 +0800

    Added: Vuejs and Tailwind, npm run dev to see changes

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+      <nav class="bg-white w-full px-[2em] py-[1em] ">
+        <div class="w-full max-w-[1300px] flex justify-between mx-auto">
+            <div><span class="font-bold">Air Quality Monitoring</span></div>
+            <div><span class="text-[0.9rem]">John Parker</span></div>
+        </div>
+    </nav>
+  </header>
+
+  <RouterView/>
+</template>
+
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,59 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <section class="mt-10 rounded grid p-4 h-[235px] w-full max-w-[1300px] bg-white mx-auto">
+        <div class="details flex w-full justify-between">
+            <div class="w-[25%] text-[.9rem]">
+                <div><span class="font-semibold text-[1.2rem]">Specification</span></div>
+                <div class="mt-3 flex justify-between">
+                    <div>             
+                        <p>Device Name</p>
+                        <p>System Voltage</p>
+                        <p>Battery Alternator</p>
+                        <p>Cylinder No.</p>
+                        <p>Type</p>
+                        <p>Intake Air Method</p>
+                    </div>
+                    <div>
+                        <p>: &nbsp&nbsp&nbspAQ Generator</p>
+                        <p>: &nbsp&nbsp&nbsp24 DCV</p>
+                        <p>: &nbsp&nbsp&nbsp80 Amps</p>
+                        <p>: &nbsp&nbsp&nbsp6</p>
+                        <p>: &nbsp&nbsp&nbspIN-line</p>
+                        <p>: &nbsp&nbsp&nbspAir-cooled</p>
+                    </div>
+                </div>
+            </div>
+            <div class="detections gap-3 grid grid-cols-3 h-[200px]">
+                <div class="bg-yellow-200 rounded flex flex-col px-6 justify-center items-center w-[230px] h-full">
+                    <p class="font-semibold">450PPM</p>
+                    <p class="text-[.8rem]">Carbon Dioxide (CO2)</p>
+                </div>
+                <div class="bg-blue-200 rounded flex flex-col px-6 justify-center items-center w-[230px] h-full">
+                    <p class="font-semibold">450PPM</p>
+                    <p class="text-[.8rem]">Carbon Dioxide (CO2)</p>
+                </div>
+                <div class="bg-red-200 rounded flex flex-col px-6 justify-center items-center w-[230px] h-full">
+                    <p class="font-semibold">450PPM</p>
+                    <p class="text-[.8rem]">Carbon Dioxide (CO2)</p>
+                </div>
+                <div class="bg-green-200 rounded flex flex-col px-6 justify-center items-center w-[230px] h-full">
+                    <p class="font-semibold">450PPM</p>
+                    <p class="text-[.8rem]">Carbon Dioxide (CO2)</p>
+                </div>
+                <div class="bg-violet-200 rounded flex flex-col px-6 justify-center items-center w-[230px] h-full">
+                    <p class="font-semibold">450PPM</p>
+                    <p class="text-[.8rem]">Carbon Dioxide (CO2)</p>
+                </div>
+                <div class="bg-yellow-200 rounded flex flex-col px-6 justify-center items-center w-[230px] h-full">
+                    <p class="font-semibold">450PPM</p>
+                    <p class="text-[.8rem]">Carbon Dioxide (CO2)</p>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+  </main>
+</template>
