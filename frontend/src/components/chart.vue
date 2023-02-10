cafef85e9c7f4e11f7fd8e178f584a4a7c64fe48
Duhell <ryanvillarma148@gmail.com>
2023-02-10 23:49:29 +0800

    remove useless code

--- frontend/src/components/chart.vue
+++ frontend/src/components/chart.vue
@@ -1,51 +1,13 @@
+<script setup>
+  import chartNav from "./chartNav.vue";
+
+</script>
+
 <template>
-  <canvas id="thisChart"></canvas>
+  <chartNav class="w-full max-w-[700px]"/>
+  <div class="bg-white p-4 max-w-[700px] rounded" id ="wrapper">
+    <canvas id="thisChart"  class="w-full max-w-[700px]" @msg="dataTransfer"></canvas>
+  </div>
+
 </template>
 
-<script>
-import Chart from "chart.js/auto";
-var dates = [];
-  for (var i = 0; i < 7; i++) {
-    dates.push(`Day ${i}`);
-  }
-window.onload = (event) => {
-  const ctx = document.getElementById("thisChart");
-  const barChart = new Chart(ctx, {
-    type: "bar",
-    data: {
-      labels: dates,
-      datasets: [
-        {
-          label: "C02",
-          data: [12, 19, 22, 11, 2, 4],
-          backgroundColor: [
-            "rgba(255,99,132,.7)",
-            "rgba(54,162,235,.7)",
-            "rgba(255,206,86,.7)",
-            "rgba(75,192,192,.7)",
-            "rgba(153,102,255,.7)",
-            "rgba(255,159,64,.7)",
-          ],
-          borderColor: [
-            "rgba(255,99,132,.7)",
-            "rgba(54,162,235,.7)",
-            "rgba(255,206,86,.7)",
-            "rgba(75,192,192,.7)",
-            "rgba(153,102,255,.7)",
-            "rgba(255,159,64,.7)",
-          ],
-          borderWidth: 1,
-        },
-      ],
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
-      },
-    },
-  });
-  barChart;
-};
-</script>
