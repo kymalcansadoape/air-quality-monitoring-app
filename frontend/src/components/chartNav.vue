144a9774054d1d510ce09dd73ca5e8a744fd3aeb
Duhell <ryanvillarma148@gmail.com>
2023-02-10 23:44:20 +0800

    Nav bar for chemicals

---
+++ frontend/src/components/chartNav.vue
@@ -0,0 +1,11 @@
+<template>
+    <nav class="text-[.9rem]">
+        <ul class="flex justify-between items-center">
+            <li><button id="co2" class="active">Carbon Dioxide</button></li>
+            <li><button id="hum">Humidity</button></li>
+            <li><button id="methane">Methane</button></li>
+            <li><button id="cm">Carbon Monoxide</button></li>
+            <li><button id="lpg">LPG</button></li>
+        </ul>
+    </nav>
+</template>
