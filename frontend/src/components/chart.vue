f047713f830aee3f0b8f11d2558abaf4c4c37172
Duhell <ryanvillarma148@gmail.com>
2023-02-11 10:18:07 +0800

    Modified the structure

--- frontend/src/components/chart.vue
+++ frontend/src/components/chart.vue
@@ -4,9 +4,11 @@
 </script>
 
 <template>
-  <chartNav class="w-full max-w-[700px]"/>
-  <div class="bg-white p-4 max-w-[700px] rounded" id ="wrapper">
-    <canvas id="thisChart"  class="w-full max-w-[700px]" @msg="dataTransfer"></canvas>
+  <div class="h-full  max-w-[700px] w-full">
+    <chartNav class="w-full"/>
+    <div class="bg-white h-[325px] w-full p-4 rounded" id ="wrapper">
+      <canvas id="thisChart"  class="w-full h-full" @msg="dataTransfer"></canvas>
+    </div>
   </div>
 
 </template>
