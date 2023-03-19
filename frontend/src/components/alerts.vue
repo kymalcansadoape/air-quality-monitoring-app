7bfe14c7509b694e5e9cbc8227ec6c0f2a1eba47
Duhell <ryanvillarma148@gmail.com>
2023-03-19 12:27:53 +0800

    new update

--- frontend/src/components/alerts.vue
+++ frontend/src/components/alerts.vue
@@ -4,52 +4,61 @@
         carbon_chart,
         temperature_chart,
         lpg_chart,
-        pressure_chart } from "../assets/fetch"
+        pressure_chart,
+        created_at } from "../assets/fetch"
     let maximumNotif = 6
     let allNotif = []
-    let notifContainerColor = "";
+    let notificationColor
     let dateToDisplay;
     setInterval(()=>{
+        let year = new Date(created_at[created_at.length - 1]).getFullYear()
+        let month = new Date(created_at[created_at.length - 1]).toLocaleDateString("en-US",{month:"long"})
+        let day = new Date(created_at[created_at.length - 1]).getDate()
+        let hours = new Date(created_at[created_at.length - 1]).getHours()
+        let mins = new Date(created_at[created_at.length - 1]).getMinutes()
         let latestRead = {
             "humidty": humidity_chart[humidity_chart.length -1],
             "lpg": lpg_chart[lpg_chart.length -1],
             "carbon": carbon_chart[carbon_chart.length -1],
             "temperature": temperature_chart[temperature_chart.length -1],
-            "pressure": pressure_chart[pressure_chart.length -1]
+            "pressure": pressure_chart[pressure_chart.length -1],
+            "DateCreated" : `${month}, ${day} ${year} - ${hours}:${mins}`
         }
-        if(latestRead.humidty >= 70){addNotif(latestRead['humidty'],"Humidity","%")
-        }else if(latestRead.lpg >= 50){addNotif(latestRead['lpg'],"LPG","PPM")}
-        else if(latestRead.carbon >= 80){addNotif(latestRead['carbon'],"Carbon Dioxide","PPM")}
-        else if(latestRead.temperature >= 37){addNotif(latestRead['temperature'],"Temperature","°C")}
-        else if(latestRead.pressure  >= 90){addNotif(latestRead['pressure'],"Pressure","hPa")}
+        if(latestRead.humidty >= 70){addNotif(latestRead['humidty'],"Humidity","%",latestRead['DateCreated'])}
+        if(latestRead.lpg >= 50){addNotif(latestRead['lpg'],"LPG","PPM",latestRead['DateCreated'])}
+        if(latestRead.carbon >= 80){addNotif(latestRead['carbon'],"Carbon Dioxide","PPM",latestRead['DateCreated'])}
+        if(latestRead.temperature >= 37){addNotif(latestRead['temperature'],"Temperature","°C",latestRead['DateCreated'])}
+        if(latestRead.pressure  >= 90){addNotif(latestRead['pressure'],"Pressure","hPa",latestRead['DateCreated'])}
         if(allNotif.length > maximumNotif){allNotif.shift()}
+
     },3000)
-    function addNotif(notif,chemicalName,unit,){
+    function addNotif(notif,chemicalName,unit,time){
         allNotif.push({
             data: notif,
             name: chemicalName,
             unit: unit,
-            timeStamp: `${new Date().toLocaleDateString("en-US",{month:"long",day:"numeric",year:"numeric"})} - ${new Date().toLocaleTimeString("en-US")}`
+            timeStamp: time
         })
         const newData = allNotif[allNotif.length - 1]['data']
         const newChemicalNameDetected = allNotif[allNotif.length - 1]['name']
-        const newTimeStamp = allNotif[allNotif.length - 1]['timeStamp']
         const newUnit = allNotif[allNotif.length - 1]['unit']
+        const newTimeStamp = allNotif[allNotif.length - 1]['timeStamp']
         const parentOfNotif = document.getElementById('main_div')
         const maxChildNotifElement = parentOfNotif.childNodes
-        console.log(maxChildNotifElement)
+        let day = new Date(created_at[created_at.length - 1])
+        alertColor(day)
         parentOfNotif.insertAdjacentHTML('afterbegin',renderNotification(newTimeStamp,newChemicalNameDetected,newData,newUnit))
-        alertColor()
         for(let j = 0; j <maxChildNotifElement.length; j++){
             if(j === 9){
                 parentOfNotif.removeChild(maxChildNotifElement[j])
                 break
             }
         }
+        
     }   
     function renderNotification(time,chemname,data,unit){
         return `
-            <div class="notifContainer bg-[#081535] h-[80px] before:rounded-l-lg relative rounded text-[.9rem] before:block before:h-full before:w-[10px] before:content-[''] before:absolute before:${notifContainerColor}">
+            <div class="notifContainer bg-[#081535] h-[80px] before:rounded-l-lg relative rounded text-[.9rem] before:block before:h-full before:w-[10px] before:content-[''] before:absolute ${notificationColor}">
                 <div class="h-full p-2 px-7">
                     <p class="text-[.8rem]">${dateToDisplay} - ${time}</p>
                     <p class="font-semibold text-[1rem]">${chemname} level exceeded the limit </p>
@@ -59,19 +68,21 @@
         `
     }
 
-    function alertColor(){
-        const now = new Date();
-        const yesterday = new Date(now);yesterday.setDate(now.getDate() - 1);
-        const lastWeek = new Date(now);lastWeek.setDate(now.getDate() - 7);
+    function alertColor(time){
+        const now = new Date(time);
+        const yesterday = new Date();
+        const lastWeek = new Date();
+        yesterday.setDate(new Date().getDate() - 1);
+        lastWeek.setDate(new Date().getDate() - 7);
         if (now.toDateString() === new Date().toDateString()) {
             dateToDisplay = "Today";
-            notifContainerColor = "bg-red-500"
-        } else if (yesterday.toDateString() === new Date().toDateString()) {
+            notificationColor = "before:bg-red-500"
+        } else if (yesterday.toDateString() === now.toDateString()) {
             dateToDisplay = "Yesterday";
-            notifContainerColor = "bg-yellow-500"
-        } else if (lastWeek < now) {
+            notificationColor = "before:bg-yellow-500"
+        } else if (lastWeek.toDateString() <= now.toDateString()) {
             dateToDisplay = "Last Week";
-            notifContainerColor = "bg-green-500"
+            notificationColor = "before:bg-green-500"
         } else {
             dateToDisplay = now.toDateString();
         }
